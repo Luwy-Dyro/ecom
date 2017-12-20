cf20d7f80dc2370ef6c5527276f6680f1d632da1
Luwypack <etx.webdesigner@gmail.com>
2017-12-19 20:36:42 -0500

    Inicio de Proyecto de CMS SIAT WEB

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp'),
+    //rename = require('gulp-rename'),
+    //notify = require('gulp-notify'),
+    //autoprefixer = require('gulp-autoprefixer'),
+    livereload = require('gulp-livereload'),
+    connect = require('gulp-connect'),
+    sass = require('gulp-sass'),
+    htmlmin = require('gulp-htmlmin'),
+    cleanCSS = require('gulp-clean-css');
+    //fileInclude = require('gulp-file-include'),
+    //minify = require('gulp-minify')
+
+var adminPath = './dist/';
+
+gulp.task('connect', function() {
+    connect.server({
+        root: 'dist/',
+        livereload: true,
+        port: 8888
+    });
+});
+
+gulp.task('estilos', function () {
+    gulp.src('./src/assets/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('./dist/assets/css'));
+});
+
+gulp.task('templates', function () {
+    gulp.src('./src/templates/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('./dist'));
+});
+
+
+gulp.task('watch', function () {
+    gulp.watch('./src/assets/scss/*.scss', ['estilos']);
+    gulp.watch('./src/templates/*.html', ['templates']);
+    // gulp.watch('./src/admin/assets/scripts/**/*.js', ['scripts:source', 'scripts:js:minify']);
+    // gulp.watch('./src/admin/assets/scripts/**/*.scss', ['scripts:css:source', 'scripts:css:minify']);
+});
+
+//gulp.task('default', ['watch']);
+gulp.task('default', ['connect', 'watch']);
